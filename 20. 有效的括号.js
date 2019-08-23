b27cd0e6cd9153d5ffa5b82f580f1bd3809e947e
hly99 <1055503066@qq.com>
2019-08-23 14:06:29 +0800

    Create 20. 有效的括号.js

---
+++ 20. 有效的括号.js
@@ -0,0 +1,11 @@
+var isValid = function (s) {
+    while (s.length) {
+        var temp = s;
+        s = s.replace('()', '');
+        s = s.replace('[]', '');
+        s = s.replace('{}', '');
+        if (s == temp){ 
+            return false}
+    }
+    return true;
+};
