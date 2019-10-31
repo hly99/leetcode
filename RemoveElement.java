a4c40517a645854aef64e54c75efb5c07f8d80ec
hly99 <1055503066@qq.com>
2019-10-31 10:39:39 +0800

    Create RemoveElement.java

---
+++ RemoveElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    	if(nums == null || nums.length == 0) {
+    		return 0;
+    	}
+    	
+    	int n = 0;
+    	for(int i = 0; i < nums.length; i++) {
+    		if(nums[i] != val) {
+    			nums[n] = nums[i];
+    			n++;
+    		}
+    	}
+    	return n;
+    }
+}
